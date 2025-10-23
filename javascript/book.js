db177e3f96c053172cd05f337b9a1af260ee99f4
Khai Pham <29khaipham@gmail.com>
2025-10-23 23:15:19 +0700

    1 chut nx

--- javascript/book.js
+++ javascript/book.js
@@ -1,3 +1,16 @@
+let allButon = document.querySelectorAll(".button");
+allButon.forEach(function (dropDown) {
+  let newBtn = dropDown.querySelector(".search-btn-text");
+  let menuList = dropDown.querySelectorAll(".dropdown-content a");
+
+  menuList.forEach((link) => {
+    link.onclick = function () {
+      let newText = this.textContent;
+      newBtn.textContent = newText;
+    };
+  });
+});
+
 function pad(n) {
   return n.toString().padStart(2, "0");
 }
@@ -7,6 +20,12 @@ function updateTime() {
   let hour = pad(now.getHours());
   let minutes = pad(now.getMinutes());
   document.getElementById("clock").textContent = `${hour}:${minutes}`;
+
+  let day = pad(now.getDate());
+  let month = pad(now.getMonth() + 1);
+  let year = now.getFullYear();
+  document.getElementById("day").textContent = `${day}-${month}-${year}`;
 }
+function updateDay() {}
 updateTime();
 setInterval(updateTime, 1000);
